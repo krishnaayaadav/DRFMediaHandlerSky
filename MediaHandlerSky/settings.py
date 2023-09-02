78684c64e249219355f95207f28d3a196bd1cbf2
krishna <krishnadj7431@gmail.com>
2023-09-02 23:02:55 +0530

    modified postgresql db is configured

--- MediaHandlerSky/settings.py
+++ MediaHandlerSky/settings.py
@@ -81,9 +81,18 @@ WSGI_APPLICATION = 'MediaHandlerSky.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME':     'drfMediaDb',
+       'USER':     'krishsky',
+       'PASSWORD': 'krish123',
+       'HOST':     'localhost',
+       'PORT':     '5432',
     }
 }
 
